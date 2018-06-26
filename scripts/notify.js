7420ee20db72894fbefd95847f760ce5fbafe862
Diego Dorgam <diego.dorgam@rocket.chat>
2018-06-26 00:50:17 -0300

    ajustes comandos | added targets all & online

--- scripts/notify.js
+++ scripts/notify.js
@@ -2,26 +2,22 @@
 //   segment rocketchat user audience and notifies them through direct message.
 // Commands:
 //   hubot authorize <user-role>
-//   hubot list - lists targets names
-//   hubot target create <target_name>
-//   hubot target delete <target_name>
-//   hubot target set <target_name>
-//   hubot user add <@username>
-//   hubot user delete <@username>
-//   hubot user list
-//   hubot send <title>\n (shift+enter)
-//	 <message>
-//   hubot report <notification_title>
-//   ------------------------------------------
-//	TODO:
-//   hubot target describe <target_name>
-//   hubot target duplicate <target_name> <target_new_name>
-//   hubot resend <all|unread|edit> <notify_id>
-//   hubot delete <notify_id>
+//   hubot list targets 
+//   hubot create target <target_name>
+//   hubot delete target <target_name>
+//   hubot set target <target_name>
+//   hubot add user <username>
+//   hubot del user <username>
+//   hubot list users
+//   hubot send <title> \n (shift+enter) <Message>
+//   hubot report <title>
 // Configuration:
 //   MONGODB_URL
 // Dependencies:
 //   hubot-iwelt-mongodb-brain
+// TODO:
+//   bot resend <all|unread|edit> <notify_id>
+//   bot delete <notify_id>
 
 module.exports = function (robot) {
 	var MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/hubot";
@@ -166,7 +162,8 @@ module.exports = function (robot) {
 			return msg.trim().substring(0, msg.length - 2);
 		}
 	}
-	const sendNotification = async function (res, msg, users){
+	const sendNotification = async function (res, users){
+		msg = res.params.message.trim();
 		receipts = [];
 		rids = [];
 		await _.each(users, async function (username) {
@@ -198,8 +195,8 @@ module.exports = function (robot) {
 		notifications.push(notification);
 		robot.brain.set("notifications", notifications);
 		return receipts;
-
 	}
+
 	// Set auth framework
 	robot.listenerMiddleware(function (context, next, done) {
 		context.response.params = context.response.params || {};
@@ -229,9 +226,7 @@ module.exports = function (robot) {
 	});
 	robot.receiveMiddleware(function (context, next, done) {
 		// check for message reading receipt
-		//console.log('MID:: context.response.message: ' + JSON.stringify(context.response));
 		if (context.response.envelope.user.roomType == "d") {
-			console.log('>>>PRIVATE MESSAGE<<<');
 			// receiving a direct message check if needs reading receipt
 			notifications = robot.brain.get('notifications');
 			for (let i = 0; i < notifications.length; i++) {
@@ -275,79 +270,142 @@ module.exports = function (robot) {
 		}
 	});
 	//LIST
-	robot.respond(/l(?:ist)?/i, { }, function (res) {
+	robot.respond(/l(?:ist) t(?:argets)?/i, { }, function (res) {
 		records = robot.brain.get('targets_by_room_' + res.envelope.room);
 		var msg = 'This are the targets you created:\n';
 		res.reply(renderTargets(res, msg, records));
 	});
 	// TARGET CREATE
-	robot.respond(/t(?:arget)? create (.+)/i, { params: 'target' }, function (res) {
-		// create a target
-		if (res.params.target){
-			targets = robot.brain.get('targets_by_room_' + res.envelope.room) || {};
-			if (res.params.target in targets) {
-				return res.reply(`Target ${res.params.target} already exists`);
+	robot.respond(/c(?:reate)? t(?:arget)? (.+)/i, { params: 'target' }, function (res) {
+		if (res.params.target) {
+			if (res.params.target.trim() == 'all' || res.params.target.trim() == 'online'){
+				return res.reply(`Target \`${res.param.target.trim()}\` is a pre-defined target, and cannot be used. Please choose a different target name.`);
 			}else{
-				targets[res.params.target] = [];
-				robot.brain.set('targets_by_room_' + res.envelope.room, targets)
-				robot.brain.set('default_target_by_room_' + res.envelope.room, res.params.target)
-				return res.reply(`Target ${res.params.target} created and set as default!`);
-			}
+				// create a target
+				targets = robot.brain.get('targets_by_room_' + res.envelope.room) || {};
+				if (res.params.target.trim() in targets) {
+					return res.reply(`Target ${res.params.target.trim()} already exists`);
+				} else {
+					targets[res.params.target.trim()] = [];
+					robot.brain.set('targets_by_room_' + res.envelope.room, targets)
+					robot.brain.set('default_target_by_room_' + res.envelope.room, res.params.target.trim())
+					return res.reply(`Target ${res.params.target.trim()} created and set as default!`);
+				}
+			} 
 		} else {
 			return res.reply(`Please specify a target name!`);
-		}	
+		}
 	});
 	// TARGET SET
-	robot.respond(/t(?:arget)? set (.+)/i, {params: 'target'}, function (res) {
+	robot.respond(/set t(?:arget)? (.+)/i, {params: 'target'}, function (res) {
 		if(!res.params.target){
 			return res.reply(`Please specify a target name`);
 		}else{
-			robot.brain.set('default_target_by_room_' + res.envelope.room, res.params.target);
-			return res.reply(`Target ${res.params.target} was set as default!`);
+			//check if target exists
+			targets = robot.brain.get('targets_by_room_' + res.envelope.room);
+			if(targets.hasOwnProperty(res.params.target.trim())){
+				// target exists, set it as default
+				robot.brain.set('default_target_by_room_' + res.envelope.room, res.params.target);
+				return res.reply(`Target ${res.params.target} was set as default!`);
+
+			}else if(res.params.target.trim() == 'all' || res.params.target.trim() == 'online'){
+
+				robot.brain.set('default_target_by_room_' + res.envelope.room, res.params.target);
+				return res.reply(`Target \`${res.params.target.trim()}\` was set as default!`);
+
+			}else{
+				// target doesn't exists
+				return res.reply(`Sorry, target \`${res.params.target.trim()}\` is not in target list!`);
+			}
 		}
 	});
 
 	// TARGET DELETE
-	robot.respond(/t(?:arget)? del(?:ete)? (.+)/i, { params: 'target' }, function (res) {
+	robot.respond(/del(?:ete)? t(?:arget)? (.+)/i, { params: 'target' }, function (res) {
 		var targets = robot.brain.get('targets_by_room_' + res.envelope.room);
-		var records = targets.filter(e => e.trim() !== res.params.target);
-		if (robot.brain.set('targets_by_room_' + res.envelope.room, records)) {
-			return res.reply(`Target ${res.params.target} was deleted!`);
+		if (res.params.target.trim() in targets) {
+			try {
+				delete targets[res.params.target.trim()];
+				robot.brain.set('targets_by_room_' + res.envelope.room, targets)
+			} catch (e) {
+				console.error('something bad happened, target couldn\'t be removed\n' + e);
+			} finally {
+				return res.reply(`Target ${res.params.target} was deleted!`);
+			}
 		} else {
-			console.error('something bad happened, target couldn\'t be removed');
+			return res.reply(`Target ${res.params.target} was not found!`);
 		}
+		
 	});
 
-	// USER ADD
-	robot.respond(/u(?:ser)? add (.+)/i, { params: 'username', requireTarget: true }, function (res) {
-		//TODO: Checks if username exists
-		//TODO: Checks if username is the sender (maybe check this on sendNotification)
+	// Add User
+	robot.respond(/add u(?:ser)? (.+)/i, { params: 'username', requireTarget: true }, async function (res) {
+		//TODO: Checks if username is the sender or the robot
 		var target_name = robot.brain.get('default_target_by_room_' + res.envelope.room);
 		var targets = robot.brain.get('targets_by_room_' + res.envelope.room);
 		new_user = res.params.username.trim();
 		if (new_user.indexOf('@') > -1){ new_user = new_user.substring('1');} 
-		if(targets[target_name].push(new_user)){
-			robot.brain.set('targets_by_room_' + res.envelope.room, targets);
-			return res.reply(`User ${res.params.username} was added to ${target_name}!`);
+		if (new_user == 'all' || new_user == 'online') {
+			temp_usernames = (new_user == 'all') ? await robot.adapter.api.users.allNames() : await robot.adapter.api.users.onlineNames();
+			let usernames = [];
+			for(key in temp_usernames){
+				usernames.push(temp_usernames[key]);
+			}
+			// add all users to target
+			let added = [];
+			let not_added = [];
+			for(user in usernames){
+				// check if User exists
+				if (targets[target_name].indexOf(usernames[user]) == -1){
+					if (targets[target_name].push(usernames[user])) {
+						robot.logger.debug(`User ${usernames[user]} added to ${target_name}\n`);
+						added.push(usernames[user]);
+					} else {
+						not_added.push(usernames[user]);
+					}
+				}else{
+					added.push(usernames[user]);
+					robot.logger.debug(`User ${usernames[user]} already exists in ${target_name}\n`);
+				}
+			}
+			msg = `From all users(${usernames.length}) I was able to add ${added.length} to ${target_name}.\n`;
+			if(not_added.length > 0) msg += `There was ${not_added.length} users I wasn't able to add to ${target_name}`;
+			return res.reply(msg);
+		}else{
+			if (targets[target_name].push(new_user)) {
+				robot.brain.set('targets_by_room_' + res.envelope.room, targets);
+				return res.reply(`User ${res.params.username} was added to ${target_name}!`);
+			}
 		}
+
 	});
 
 	// User delete
-	robot.respond(/u(?:ser)? del(?:ete)? (.+)/i, { params: 'username', requireTarget: true }, function (res) {
+	robot.respond(/del(?:ete)? u(?:ser)? (.+)/i, { params: 'username', requireTarget: true }, function (res) {
 		var target_name = robot.brain.get('default_target_by_room_' + res.envelope.room);
 		var targets = robot.brain.get('targets_by_room_' + res.envelope.room);
 		var records = targets[target_name];
-		targets[target_name] = records.filter(e => e.trim() !== res.params.username);
-		 
-		if (robot.brain.set('targets_by_room_' + res.envelope.room, targets)) {	
-			return res.reply(`User ${res.params.username} was delete from ${target_name}!`);
+		if (records.indexOf(res.params.username.trim()) > -1){
+			try {
+				delete records[records.indexOf(res.params.username.trim())];
+				targets[target_name] = records;
+				robot.brain.set('targets_by_room_' + res.envelope.room, targets);
+			} catch (e) {
+				console.error('something bad happened, user couldn\'t be removed');
+			}finally{
+				return res.reply(`User ${res.params.username} was delete from ${target_name}!`);
+			}
 		} else {
-			console.error('something bad happened, user couldn\'t be removed');
+			return res.reply(`User ${res.params.username} was not found in ${target_name}!`);
 		}
+		
+		
 	});
 
 	// User list
-	robot.respond(/u(?:ser)? list/i, { requireTarget: true }, function (res) {
+	robot.respond(/l(?:ist) u(?:sers)?/i, {
+				requireTarget: true
+			}, function (res) {
 		defaultTarget = robot.brain.get('default_target_by_room_'+res.envelope.room);
 		if(defaultTarget){
 			var targets = robot.brain.get('targets_by_room_' + res.envelope.room);
@@ -363,15 +421,32 @@ module.exports = function (robot) {
 	robot.respond(/s(?:end)?\s(.+)\s*\n?((?:(.|\n)*\n?)*)/i, { params: 'title, message', requireTarget: true }, async function (res) {
 		defaultTarget = robot.brain.get('default_target_by_room_'+res.envelope.room);
 		if(defaultTarget){
-			var targets = robot.brain.get('targets_by_room_' + res.envelope.room);
-			var records = targets[defaultTarget];
-			var records = records.filter(e => e.trim() != res.envelope.user.name);
-			if(res.params.message){
-				res.reply(`Your message:\n${res.params.title}\nis being sended to:\n${records}`);
-				returned_receipts = await sendNotification(res, res.params.message, records);
-				if (returned_receipts.length > 0) return res.reply(`Your message has been sended`);
+			if (defaultTarget == 'all' || defaultTarget == 'online'){
+				var temp_records = (defaultTarget == 'all') ? await robot.adapter.api.users.allNames() : await robot.adapter.api.users.onlineNames();
+				let records = [];
+				for (key in temp_records) {
+					records.push(temp_records[key]);
+				}
+			}else{
+				var targets = robot.brain.get('targets_by_room_' + res.envelope.room);
+				var records = targets[defaultTarget];
 			}
-
+			if (records.indexOf(res.envelope.user.name) > -1) records.pop(res.envelope.user.name);
+			robot_name = robot.alias || robot.name;
+			if (records.indexOf(robot_name) > -1) records.pop(robot_name);
+			if (res.params.message) {
+				try{
+					res.reply(`Your message:\n${res.params.title}\nis being sended to:\n${records}`);
+					returned_receipts = await sendNotification(res, records);
+					if (returned_receipts.length > 0) {
+						return res.reply(`Your message has been sent to ${returned_receipts.length} users`);
+					} else {
+						return res.reply(`Your wasn't sent to anyone, please verify the logs for more details`);
+					}
+				}catch (e){
+					console.error(e);
+				}
+			}
 		} else {
 			return res.reply(getSetTargetMessage());
 		}
